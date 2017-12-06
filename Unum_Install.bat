7e42f9547a9a3f126f49e537324dde492914ca4e
Gator Boy <Gator-Boy11@users.noreply.github.com>
2017-12-06 09:41:40 -0500

    Update Unum_Install.bat
    Includes readme and license in the download.

--- Unum_Install.bat
+++ Unum_Install.bat
@@ -3,6 +3,8 @@ mkdir Unum
 cd Unum
 bitsadmin.exe /transfer "Download" /Download https://raw.githubusercontent.com/Gator-Boy11/Unum/master/unum_client.py %cd%\unum_client.py
 bitsadmin.exe /transfer "Download" /Download https://raw.githubusercontent.com/Gator-Boy11/Unum/master/unum_server.py %cd%\unum_server.py
+bitsadmin.exe /transfer "Download" /Download https://raw.githubusercontent.com/Gator-Boy11/Unum/master/LICENSE %cd%\LICENSE.txt
+bitsadmin.exe /transfer "Download" /Download https://raw.githubusercontent.com/Gator-Boy11/Unum/master/README.md %cd%\README.md
 bitsadmin.exe /transfer "Download" /Download https://www.python.org/ftp/python/3.6.3/python-3.6.3-amd64.exe %cd%\python-3.6.3-amd64.exe
-python-3.6.3-amd64.exe /passive PrependPath=1 InstallAllUsers=0 InstallLauncherAllUsers=0
+python-3.6.3-amd64.exe /passive PrependPath=0 InstallAllUsers=0 InstallLauncherAllUsers=0 DefaultJustForMeTargetDir=%USERPROFILE%\Documents
 del %cd%\python-3.6.3-amd64.exe
