b73013c171556e5bc398cd00e7624a774626a61c
Gator Boy <Gator-Boy11@users.noreply.github.com>
2017-12-11 10:05:10 -0500

    Update Unum_Install.bat
    Added Compatibility with x86 systems, so the better python version will be installed.

--- Unum_Install.bat
+++ Unum_Install.bat
@@ -5,6 +5,8 @@ bitsadmin.exe /transfer "Download" /Download https://raw.githubusercontent.com/G
 bitsadmin.exe /transfer "Download" /Download https://raw.githubusercontent.com/Gator-Boy11/Unum/master/unum_server.py %cd%\unum_server.py
 bitsadmin.exe /transfer "Download" /Download https://raw.githubusercontent.com/Gator-Boy11/Unum/master/LICENSE %cd%\LICENSE.txt
 bitsadmin.exe /transfer "Download" /Download https://raw.githubusercontent.com/Gator-Boy11/Unum/master/README.md %cd%\README.md
-bitsadmin.exe /transfer "Download" /Download https://www.python.org/ftp/python/3.6.3/python-3.6.3-amd64.exe %cd%\python-3.6.3-amd64.exe
-python-3.6.3-amd64.exe /passive PrependPath=0 InstallAllUsers=0 InstallLauncherAllUsers=0 DefaultJustForMeTargetDir=%USERPROFILE%\Documents
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
+if %OS%==64BIT bitsadmin.exe /transfer "Download" /Download https://www.python.org/ftp/python/3.6.3/python-3.6.3-amd64.exe %cd%\python-3.6.3.exe
+if %OS%==32BIT bitsadmin.exe /transfer "Download" /Download https://www.python.org/ftp/python/3.6.3/python-3.6.3.exe %cd%\python-3.6.3.exe
+python-3.6.3.exe /passive PrependPath=0 InstallAllUsers=0 InstallLauncherAllUsers=0 DefaultJustForMeTargetDir=%USERPROFILE%\Documents
 del %cd%\python-3.6.3-amd64.exe
